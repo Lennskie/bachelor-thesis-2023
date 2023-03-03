a597001ab53f9dddec2661df6d07bc86004979fb
Lennskie <lenn.crochart@gmail.com>
2023-03-03 10:14:59 +0100

    Working basic POC

--- poc.py
+++ poc.py
@@ -1,8 +1,5 @@
-
 import os
 import time
-
-# Create a placeholder file named "honeytoken filled with fake data. This is done with pandas because it is fast. 
 
 # Take user input and set it to the filename variable
 
@@ -26,16 +23,18 @@ time.sleep(5)
 # use os.path.getatime(path) to check the last access time of the file
 
 last_access_time = os.path.getatime(filename)
-print("The last access time of " + filename + " is " + last_access_time)
+print("The last access time of " + filename + " is ")
+print(last_access_time)
 
 while True:
     # check the last access time of the file every 5 seconds
     time.sleep(5)
-    last_access_time = os.path.getatime(filename)
-    print("The last access time of " + filename + " is " + last_access_time)
+    _last_access_time = os.path.getatime(filename)
+    print("The last access time of " + filename + " is ")
+    print(_last_access_time)
 
     # if the last access time is greater than 5 seconds, then the file has been accessed
-    if(last_access_time > 5):
+    if(_last_access_time > last_access_time):
         print("The file has been accessed")
         break
 
