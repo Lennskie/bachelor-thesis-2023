9e87f10704132d60728a592dbd9d5357e03ec175
Lennskie <lenn.crochart@gmail.com>
2023-04-03 17:41:27 +0200

    Possible solution to access bug. TODO: test

--- python/make_json.py
+++ python/make_json.py
@@ -1,6 +1,8 @@
 import json
 import os
 import win32api
+import win32evtlog
+import win32security
 
 def make_json(filename, last_access_time, _uuid):
 
@@ -37,6 +39,27 @@ def make_json(filename, last_access_time, _uuid):
             file.write(str(json.dumps(_json)))
             file.close()
 
+def get_last_access_user(file_path):
+    user_name = None
+    handle = win32evtlog.OpenEventLog(None, 'Security')
+    flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
+    events = win32evtlog.ReadEventLog(handle, flags, 0)
+    for event in events:
+        if event.EventType == win32evtlog.EVENTLOG_AUDIT_SUCCESS and event.SourceName == 'Microsoft Windows security auditing.' and file_path in event.StringInserts:
+            for string in event.StringInserts:
+                if string.startswith('Subject:'):
+                    user_sid = string.split('Security ID: ')[1].split('\n')[0]
+                    user_name, _, _ = win32security.LookupAccountSid(None, win32security.ConvertStringSidToSid(user_sid))
+                elif string.startswith('Accesses:'):
+                    if 'ReadData' in string:
+                        break
+            break
+    win32evtlog.CloseEventLog(handle)
+    return user_name
+
+
+
+
 def update_json(_uuid):
     # open the json file and update the accessed value to True
     with open("files.json", "r") as file:
@@ -45,7 +68,7 @@ def update_json(_uuid):
         for i in range(len(_json)):
             if(_json[str(i)]["uuid"] == _uuid):
                 _json[str(i)]["accessed"] = True
-                _json[str(i)]["by"] = win32api.GetUserName()
+                _json[str(i)]["by"] = get_last_access_user(_json[str(i)]["path"])
         with open("files.json", "w") as file: # reopen in write mode
             file.write(str(json.dumps(_json)))
             file.close()
